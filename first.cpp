e612a45cb355958c47a795a5bf86c0a5647da333
chitte <chitterao123@gmail.com>
2019-04-03 18:04:53 +0530

    first comit

---
+++ first.cpp
@@ -0,0 +1,9 @@
+
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello ubuntu" << std::endl;
+	return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,14 @@
+
+
+#include<iostream>
+
+int main()
+{
+
+	int num;
+
+	std::cout << "hello there enter num " << std::endl;
+	std::cin >> num;
+	std::cout << num;
+	return 0;
+}
